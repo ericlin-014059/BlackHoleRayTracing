finishing the trajectory; fixing the sign of angular momentum

massive_geodesics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 class massive_geodesics:
     def __init__(self, b, v, M):
@@ -17,16 +18,21 @@ class massive_geodesics:
 
         self.gamma = 1 / np.sqrt(1 - v ** 2)
         self.E = self.gamma
-        self.L = self.gamma * b * v
+        self.L = -self.gamma * b * v
 
         self.central_mass = M
 
         self.ingoing = True
 
-        self.r = 50
-        self.phi = np.pi
+        self.r = 500
+        self.phi = np.pi - np.arcsin(b / self.r)
 
         self.r_critical = self._critical_radius(M)
+
+        self.x_positions = [] 
+        self.y_positions = []
+
+        self._run_trajectory()
 
     def get_cartesian(self):
         x = self.r * np.cos(self.phi)
@@ -47,14 +53,45 @@ class massive_geodesics:
 
         return np.max(np.real(real_roots)).item()
 
-    def check_in_or_out(self):
-        return abs(self.r - self.r_critical) < 1e-4
+    def arrive_critical(self):
+        return abs(self.r - self.r_critical) > 1e-4
     
-    def trajectory(self, step = 0.01):
-        while (self.phi < 0) and (self.r < 50):
-            self.ingoing = self.check_in_or_out()
+    def _run_trajectory(self, step = 0.01):
+        while self.r <= 500:
+            dr = np.sqrt(self.E ** 2 - (1 - 2 * self.central_mass / self.r) * (1 + self.L ** 2 / self.r ** 2)) * step
 
-            dr = np.sqrt(self.E ** 2 - (1 - 2 * self.central_mass) / self.r)
-            dphi = self.L / self.r ** 2
-    
-    
+            dphi = self.L / self.r ** 2 * step
+
+            if self.ingoing: 
+                self.r -= dr
+                self.ingoing = self.arrive_critical()
+
+            else:
+                self.r += dr
+
+            self.phi += dphi
+
+            self.phi = (self.phi + np.pi) % (2 * np.pi) - np.pi 
+
+            x, y = self.get_cartesian()
+
+            self.x_positions.append(x)
+            self.y_positions.append(y)
+
+# you can uncomment the following lines to test if everything works good
+
+# fig, ax = plt.subplots(figsize = (6, 6))
+
+# ax.set_xlim(-10, 10)
+# ax.set_ylim(-10, 10)
+
+# M = 1
+# r = 2 * M
+# circle = Circle((0, 0), r, color = "black")
+
+# test = massive_geodesics(7, 0.9, M)
+
+# ax.add_patch(circle)
+# ax.plot(test.x_positions, test.y_positions)
+
+# plt.savefig("test.png")